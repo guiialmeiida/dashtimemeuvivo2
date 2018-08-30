0aeca3b12ba475fd5548bc12decb3b4793370215
JoaoFilippio <41478547+JoaoFilippio@users.noreply.github.com>
2018-08-30 10:18:18 -0300

    Update AvisoN1Full.user.js

--- AvisoN1Full.user.js
+++ AvisoN1Full.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @name         Aviso Dashboard
+// @name         Aviso Jao
 // @version      2.3 Beta
 // @description  Notificação dos defeitos, opções layout
-// @author       Thauã Corrêa Martins, Eduardo Czamanski Rota
-// @source       https://github.com/correamth/dashaviso
+// @author       Execifico Jao
+// @source       https://github.com/JoaoFilippio/dashtimemeuvivo2
 // @match        */controle-demanda/defectMonitoring*
 // @grant        GM_notification
 // @grant        window.focus
-// @require      https://raw.githubusercontent.com/correamth/dashaviso/master/AvisoN1Full.js
+// @require      https://raw.githubusercontent.com/JoaoFilippio/dashtimemeuvivo2/master/AvisoN1Full.js
 // @icon         https://forum.vivo.com.br/attachment.php?attachmentid=737&d=1418039499
 // ==/UserScript==
